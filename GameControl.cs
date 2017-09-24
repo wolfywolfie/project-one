145b0fac4cc7532832039edd5f3350f7f16b1b33
wolfywolfie <31907254+wolfywolfie@users.noreply.github.com>
2017-09-24 13:43:21 -0400

    Add files via upload
    Part 2

---
+++ GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+	bool Message;
+
+	// Use this for initialization
+	void Start () {
+		print("Game Start~!");
+		Message = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > 3 && Message == false) {
+			print("It has been three seconds! uwu");
+			Message = true;
+			}
+
+	}
+}
